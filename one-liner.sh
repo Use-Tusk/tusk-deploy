bef6510ab211cf5b2901c209302b75cf0c069584
podocarp <xdjiaxd@gmail.com>
2025-07-11 10:26:05 +0800

    update script

--- one-liner.sh
+++ one-liner.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/Use-Tusk/tusk-deploy.git .
-
+git clone https://github.com/Use-Tusk/tusk-deploy.git tusk-deploy
+echo "cd tusk-deploy" >> ~/.bashrc
+cd tusk-deploy
 ./install.sh
-
 exec sg docker "docker compose up"
